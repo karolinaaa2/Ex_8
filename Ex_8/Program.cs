c467da834782e270d24b39aeb3b2189f618e412a
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:40:54 +0300

    Add project files.

---
+++ Ex_8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть довжини сторін трикутника:");
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+
+        double longest = Math.Max(a, Math.Max(b, c));
+        Console.WriteLine($"Найдовша сторона: {longest}");
+    }
+}
